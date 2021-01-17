e509cffcbcbbb13ac0e3f8b469294f0651a01367
entirelymagic@gmail.com <@lexandr@123>
2021-01-17 19:43:04 +0200

    Added notes for inserting and deleting from DB.

--- Notes/notes_03.py
+++ Notes/notes_03.py
@@ -18,3 +18,29 @@ pipeline = [
 aggregation = movies.aggregate(pipeline)
 print(dumps(aggregation, indent=2))
 
+"""
+    Upserts vs. Updates
+    Sometimes, we want to update a document, but we're not sure if it exists in the collection.
+    
+    We can use an "upsert" to update a document if it exists, and insert it if it does not exist.
+    
+    In the following example, we're not sure if this video game exists in our collection, but we want to make sure there 
+is a document in the collection that contains the correct data. 
+    
+    This operation may do one of two things:
+    
+    If the predicate matches a document, update the document to contain the correct data.
+    
+    If the document doesn't exist, create the desired document.
+"""
+
+# -----------------------------------------------------
+
+"""Unlike delete_one, delete_many deletes all documents that match the supplied predicate. Because of this behavior, 
+delete_many is a little more "dangerous". 
+
+    - delete_one will delete the first document that matches the supplied predicate. 
+    - delete_many will delete all documents matching the supplied predicate. 
+    - The number of documents deleted can be accessed via the deleted_count property on the DeleteResult object
+     returned from a delete operation. 
+"""
